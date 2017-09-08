4b37064a141086b308667dcb2891205cb8302416
kamil_j <kamil.jurczak@gmail.com>
2017-09-08 18:12:38 +0200

    spring - annotation based configuration

---
+++ webLibrary-application/src/main/java/pl/kamilj/webLibrary/application/web/configuration/WebConfiguration.java
@@ -0,0 +1,24 @@
+package pl.kamilj.webLibrary.application.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.kamilj.webLibrary.application.web.controller")
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    ViewResolver jspViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/jsp/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
---
+++ webLibrary-application/src/main/webapp/WEB-INF/views/jsp/main.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<h2>Hello to WebLibrary application!</h2>
+</body>
+</html>
